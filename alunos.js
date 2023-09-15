f33a0374e5cca432ec072bedcc444d8b4e9c085d
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-09-15 01:50:54 +0000

    banco de dados alunos.js

--- alunos.js
+++ alunos.js
@@ -9,8 +9,6 @@ const readline = require('readline-sync');
 //   }
 // }
 
-
-let cadastroDeMatricula = 2;
 const aluno1 = {
   nome: "Ana Marinho",
   telefone: "81 98888-9997",
@@ -26,123 +24,120 @@ const aluno2 = {
 
 const alunos = [aluno1, aluno2];
 
-
-
 // MENU DO ALUNO
 let loop = true;
 let alunoBusca;
 while (loop) {
 	console.log('\n********** ÁREA DO ALUNO: **********');
 	console.log('\nOPÇÃO:');
-	console.log('1. Cadastrar acesso:');
-	console.log('2. Exibir informações cadastradas:');
-	console.log('3. Buscar um instrumento:');
-	console.log('4. Alterar cadastro:');
-	console.log('5. Excluir cadastro:');
+	console.log('1. Cadastro de aluno:');
+	console.log('2. Exibir cadastro do aluno:');
+	console.log('3. Buscar um aluno:');
+	console.log('4. Alterar cadastro do aluno:');
+	console.log('5. Excluir cadastro do aluno:');
 	console.log('0. Sair');
 	console.log('*******************************************');
 	const opcao = readline.questionInt('Escolha uma opção: ');
 
     switch (opcao) {
-        
-        //OPÇAO 1. CREATE - (CADASTRO DO ALUNO NO SISTEMA)
-        case 1:
-        let nomeAluno = readline.question("Digite seu nome:");
-        let telefoneAluno = readline.questionInt("Digite seu telefone:");
-        let emailAluno = readline.question("Digite seu melhor e-mail:");
-        let turmaAluno = readline.questionInt("Qual sua turma? (1)- Sábado iniciante (2)- Sábado avançado (3)- Domingo iniciante (4)- Domingo avançado");
-        cadastroDeMatricula++;
-        const aluno = {
-        nome: nomeAluno,
-        telefone: telefoneAluno,
-        email: emailAluno,
-        turma: turmaAluno,
-        };
-        alunos.push(aluno);
-        console.log("Cadastro feito com sucesso!");
-        readline.keyInPause();
-        break;
+    //OPÇAO 1. CREATE - (CADASTRO DO ALUNO NO SISTEMA)
+    case 1:
+    let nomeAluno = readline.question("Digite seu nome:");
+    let telefoneAluno = readline.questionInt("Digite seu telefone:");
+    let emailAluno = readline.question("Digite seu melhor e-mail:");
+    let turmaAluno = readline.questionInt("Qual sua turma? (1)- Sábado iniciante (2)- Sábado avançado (3)- Domingo iniciante (4)- Domingo avançado");
+    
+    const aluno = {
+    nome: nomeAluno,
+    telefone: telefoneAluno,
+    email: emailAluno,
+    turma: turmaAluno,
+    };
 
-        // OPÇAO 2. READ - (O ALUNO VISUALIZAR SEU CADASTRO NO SISTEMA)
-        case 2:
-        alunoBusca = readline.question("Digite seu e-mail:");
+    alunos.push(aluno);
+    console.log("Cadastro feito com sucesso!");
+    readline.keyInPause();
+    break;
+
+    // OPÇAO 2. READ - (VISUALIZAR O CADASTRO DO ALUNO NO SISTEMA)
+    case 2:
+      console.log("Listando todos os alunos");
+      console.log("------------------------");
+      for (const a of alunos) {
+        console.log(`Nome: ${a.nome}`);
+        console.log(`Telefone: ${a.telefone}`);
+        console.log(`E-mail: ${a.email}`);
+        console.log(`Turma: ${a.turma}`);
+        console.log("------------------------");
+      }
+    readline.keyInPause();
+    break;  
+
+    // OPÇAO 3. BUSCAR - (BUSCA UM ALUNO DO SISTEMA)
+    case 3:
+    alunoBusca = readline.question("Digite nome do aluno: ");
+      for (const a of alunos) {
+        if (a.nome === alunoBusca) {
+          console.log("Resultado da busca");
+          console.log("------------------------");
+          console.log(`Nome: ${a.nome}`);
+          console.log(`Telefone: ${a.telefone}`);
+          console.log(`E-mail: ${a.email}`);
+          console.log(`Turma: ${a.turma}`);   
+        }
+      }
+    readline.keyInPause();
+    break;
+        
+    // OPÇAO 4. UPDATE - (ALTERAR INFORMAÇOES DO CADASTRO NO SISTEMA)  
+    case 4:
+      alunoBusca = readline.question("Digite nome do aluno: ");
         for (const a of alunos) {
-            if (a.login === alunoBusca) {
-            console.log("Resultado da busca");
-            console.log("------------------------");
-            console.log(`Matricula: ${a.matricula}`);
-            console.log(`Nome: ${a.nome}`);
-            console.log(`Telefone: ${a.telefone}`);
-            console.log(`E-mail: ${a.login}`);
-            console.log(`Turma: ${a.turma}`);   
-            }
-        }
-        
-          readline.keyInPause();
-          break;
-        
-        // OPÇAO 3. BUSCAR UM INSTRUMENTO - (O ALUNO SABER SE O INSTRUMENTO ESTA DISPONÍVEL)
-
-        // OPÇAO 4. UPDATE - (O ALUNO ALTERAR SEU CADASTRO NO SISTEMA)  
-        case 4:
-            alunoBusca = readline.question("Digite seu e-mail: ");
-          for (const a of alunos) {
-            if (a.login === alunoBusca) {
-            while (true) {
-              try {
-                let alterarDados = readline.questionInt(`Qual opção você quer alterar: (1) Telefone, (2) E-mail, (3) Senha: `);
-                if (alterarDados == 1) {
-                  a.telefone = readline.questionInt(`Digite o telefone: `);
-                }
-                else if (alterarDados == 2) {
-                  a.login = readline.question(`Digite o e-mail: `);
-                }
-                else if (alterarDados == 3) {
-                  a.senha = readline.questionInt(`Digite uma nova senha com 4 números: `);
-                }
-              } catch (e){ 
-                console.log("Digite apenas as opções citadas. "); 
+          if (a.nome === alunoBusca) {
+          while (true) {
+            try {
+              let alterarDados = readline.questionInt(`Qual opção você quer alterar: (1) Telefone, (2) E-mail, (3) Turma: `);
+              if (alterarDados == 1) {
+                a.telefone = readline.questionInt(`Digite o telefone: `);
+                break;
               }
-
-              try {
-                let finalizarAlteracao = readlineSync.question('Deseja finalizar a alteração? Digite S para Encerrar a Eleição ou N para Continuar a Eleição:');
-        if (finalizarAlteracao != 'S'){
-            throw Error ('Digite novamente:');
-        }
-
-        if (finalizarAlteracao = 'S'){
-          console.log("Cadastro alterado com sucesso!");
-              } 
-            } catch (e) {
-              console.log('Digite qual opção novamente:');   
+              else if (alterarDados == 2) {
+                a.email = readline.question(`Digite o e-mail: `);
+              }
+              else if (alterarDados == 3) {
+                a.turma = readline.questionInt(`Digite a turma: (1)- Sábado iniciante (2)- Sábado avançado (3)- Domingo iniciante (4)- Domingo avançado `);
+              }
+            } catch (e){ 
+              console.log("Digite apenas as opções citadas. "); 
+              }
+             console.log("Cadastro alterado com sucesso!");
           } 
           } 
         }
-       } readline.keyInPause();
-        break;
-        
-
-        // OPÇAO 5. DELETE - (O ALUNO EXCLUIR SEU CADASTRO NO SISTEMA)  
-        case 5:
-            alunoBusca = readline.questionInt("Digite seu e-mail: ");
-          for (const a of alunos) {
-            if (a.login === alunoBusca) {
-              console.log(a.nome);
-              console.log(`Excluido aluno ${a.nome} do sistema..."`);
-              alunos.splice(a, 1);
-            }
+      readline.keyInPause();
+      break;
+       
+      // OPÇAO 5. DELETE - (EXCLUIR O ALUNO DO CADASTRO NO SISTEMA)  
+      case 5:
+      alunoBusca = readline.question("Digite nome do aluno: ");
+        for (const a of alunos) {
+          if (a.nome === alunoBusca) {
+            console.log(a.nome);
+            console.log(`Excluido aluno ${a.nome} do sistema..."`);
+            alunos.splice(a, 1);
           }
-          console.log("Aluno excluído com sucesso!");
-          readline.keyInPause();
-          break;
+        }
+      console.log("Aluno excluído com sucesso!");
+      readline.keyInPause();
+      break;
          
-        // OPÇÃO 0. SAIR DO SISTEMA  
-        case 0:
-          console.log("Saindo do sistema...");
-          loop = false;
-          break;
-        default:
-          console.log("Opção invalida!");
-          break;
+      // OPÇÃO 0. SAIR DO SISTEMA  
+      case 0:
+      console.log("Saindo do sistema...");
+      loop = false;
+      break;
+      default:
+      console.log("Opção invalida!");
+      break;
       }
     }
