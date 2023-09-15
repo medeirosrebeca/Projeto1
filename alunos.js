328fffca7944d3257138fbcab56c2428bffffd42
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2023-09-15 23:49:49 +0000

    crud aluno_projeto

--- alunos.js
+++ alunos.js
@@ -1,28 +1,19 @@
 const readline = require('readline-sync');
 
-// class aluno{
-//   constructor(nome, telefone, email, turma) {
-//   this.nome = this.nome;
-//   this.telefone = this.telefone;
-//   this.email = this.email;
-//   this.turma = this.turma;
-//   }
-// }
+// const aluno1 = {
+//   matricula: 1,
+//   nome: "Ana Marinho",
+//   telefone: "81 98888-9997",
+//   email: "aluna@aluno.com",
+//   turma: "Sábado iniciante",
+// };
 
-const aluno1 = {
-  matricula = 1;
-  nome: "Ana Marinho",
-  telefone: "81 98888-9997",
-  email: "aluna@aluno.com",
-  turma: "Sábado iniciante",
-};
-
-const bancoDeAlunos = [aluno1];
+const bancoDeAlunos = [];
 
 // MENU DO ALUNO
 let loop = true;
 let alunoBusca;
-let matriculaAluno;
+let matricula = 0;
 
 while (loop) {
 	console.log('\n********** ÁREA DO ALUNO: **********');
@@ -42,15 +33,32 @@ while (loop) {
     let nomeAluno = readline.question("Digite seu nome:");
     let telefoneAluno = readline.questionInt("Digite seu telefone:");
     let emailAluno = readline.question("Digite seu melhor e-mail:");
-    let turmaAluno = readline.questionInt(`Qual sua turma? (1)- Sábado iniciante (2)- Sábado avançado, (3)- Domingo iniciante, (4)- Domingo avançado`);
-    turmaAluno = 
-
+    let turmaResultado = readline.questionInt(`Qual sua turma? (1)- Sábado iniciante (2)- Sábado avançado, (3)- Domingo iniciante, (4)- Domingo avançado`);
+    
+    switch (turmaResultado) {
+      case 1:
+        turmaAluno = "Sábado iniciante";
+        break;
+      case 2:
+        turmaAluno = "Sábado avançado";
+        break;
+      case 3:
+        turmaAluno = "Domingo iniciante";
+        break;
+      case 4:
+        turmaAluno = "Domingo avançado";
+        break;
+      default:
+        turmaAluno ="Opção inválida";
+        break;
+      }
+    matricula++;
     const aluno = {
-    matricula: matricula, //*****************
+    matricula: matricula, 
     nome: nomeAluno,
     telefone: telefoneAluno,
     email: emailAluno,
-    turma: toString(turmaAluno), //*************
+    turma: turmaAluno, 
     };
 
     bancoDeAlunos.push(aluno);
@@ -63,6 +71,7 @@ while (loop) {
       console.log("Listando todos os alunos");
       console.log("------------------------");
       for (const a of bancoDeAlunos) {
+        console.log(`Matricula: ${a.matricula}`);
         console.log(`Nome: ${a.nome}`);
         console.log(`Telefone: ${a.telefone}`);
         console.log(`E-mail: ${a.email}`);
@@ -74,15 +83,16 @@ while (loop) {
 
     // OPÇAO 3. BUSCAR - (BUSCA UM ALUNO DO SISTEMA)
     case 3:
-    alunoBusca = readline.question("Digite nome do aluno: ");
+    alunoBusca = readline.questionInt("Digite matricula do aluno: ");
       for (const a of bancoDeAlunos) {
-        if (a.nome === alunoBusca) {
+        if (a.matricula === alunoBusca) {
           console.log("Resultado da busca");
           console.log("------------------------");
           console.log(`Nome: ${a.nome}`);
           console.log(`Telefone: ${a.telefone}`);
           console.log(`E-mail: ${a.email}`);
-          console.log(`Turma: ${a.turma}`);   
+          console.log(`Turma: ${a.turma}`);  
+          console.log("------------------------"); 
         }
       }
     readline.keyInPause();
@@ -90,9 +100,9 @@ while (loop) {
         
     // OPÇAO 4. UPDATE - (ALTERAR INFORMAÇOES DO CADASTRO NO SISTEMA)  
     case 4:
-      alunoBusca = readline.question("Digite nome do aluno: ");
+      alunoBusca = readline.questionInt("Digite matrícula do aluno: ");
         for (const a of bancoDeAlunos) {
-          if (a.nome === alunoBusca) {
+          if (a.matricula === alunoBusca) {
           while (true) {
             try {
               let alterarDados = readline.questionInt(`Qual opção você quer alterar: (1) Telefone, (2) E-mail, (3) Turma: `);
@@ -102,15 +112,19 @@ while (loop) {
               }
               else if (alterarDados == 2) {
                 a.email = readline.question(`Digite o e-mail: `);
+                break;
               }
               else if (alterarDados == 3) {
                 a.turma = readline.questionInt(`Digite a turma: (1)- Sábado iniciante (2)- Sábado avançado (3)- Domingo iniciante (4)- Domingo avançado `);
+                break;
+              } else if (alterarDados != 1 && alterarDados != 2 && alterarDados != 3){
+                throw Error ("Opção inválida");
               }
             } catch (e){ 
               console.log("Digite apenas as opções citadas. "); 
               }
-             console.log("Cadastro alterado com sucesso!");
-          } 
+          }
+              console.log("Cadastro alterado com sucesso!"); 
           } 
         }
       readline.keyInPause();
@@ -118,12 +132,12 @@ while (loop) {
        
       // OPÇAO 5. DELETE - (EXCLUIR O ALUNO DO CADASTRO NO SISTEMA)  
       case 5:
-      alunoBusca = readline.question("Digite nome do aluno: ");
+      alunoBusca = readline.questionInt("Digite matrícula do aluno: ");
         for (const a of bancoDeAlunos) {
-          if (a.nome === alunoBusca) {
+          if (a.matricula === alunoBusca) {
             console.log(a.nome);
             console.log(`Excluido aluno ${a.nome} do sistema..."`);
-            alunos.splice(a, 1);
+            bancoDeAlunos.splice(a, 1);
           }
         }
       console.log("Aluno excluído com sucesso!");
